6347064565fc500a269a87387ba472b5a96b440d
Nagisa Tateishi <nattou0fx@gmail.com>
2026-01-21 14:52:00 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+  autoplay: true,
+	dots: true,
+	arrows: true,
+	variableWidth: true, //左右のスライドを見せるために必要
+	centerMode: true, //スライドを中心に寄せる
+	slidesToShow: 4, //スライドの見える枚数に合わせて入れると良い
+});
