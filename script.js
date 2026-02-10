4afcf950a1b8eb79e8915b5611acb103ce6098ac
Nagisa Tateishi <nattou0fx@gmail.com>
2026-02-10 09:56:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ $('.slider').slick({
 	arrows: true,
 	variableWidth: true, //左右のスライドを見せるために必要
 	centerMode: true, //スライドを中心に寄せる
-	slidesToShow: 4, //スライドの見える枚数に合わせて入れると良い
+	slidesToShow: 3, //スライドの見える枚数に合わせて入れると良い
 });
